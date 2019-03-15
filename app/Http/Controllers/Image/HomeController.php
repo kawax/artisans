0759f1f44e6a4336ffe28de62293f656dead6123
kawax <kawaxbiz@gmail.com>
2019-03-15 21:55:44 +0900

    image

--- app/Http/Controllers/Image/HomeController.php
+++ app/Http/Controllers/Image/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
     {
         $img = Image::canvas(800, 400, config('artisans.primary'));
 
-        $img->text(config('app.name'), 100, 170, function ($font) {
+        $img->text(config('app.name'), 120, 170, function ($font) {
             $font->file(config('artisans.font'));
             $font->size(64);
             $font->color('#fff');
