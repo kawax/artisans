8fcf6682303405c60c1d6d67c9f7b4b38ccc29b2
kawax <kawaxbiz@gmail.com>
2019-03-16 18:04:50 +0900

    card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,5 @@
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:creator" content="{{ $name ?? '' }}">
-<meta name="og:title" content="{{ $title ?? config('app.name', 'Laravel') }}">
-<meta name="og:description" content="{{ $description ?? '' }}">
-<meta name="og:image" content="{{ $image ?? route('image.home') }}">
+<meta property="og:title" content="{{ $title ?? config('app.name', 'Laravel') }}">
+<meta property="og:description" content="{{ $description ?? '' }}">
+<meta property="og:image" content="{{ $image ?? route('image.home') }}">
