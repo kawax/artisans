6f794911932bc7a210c4bc0be762ab22a254b5e0
kawax <kawaxbiz@gmail.com>
2019-03-21 20:07:23 +0900

    font

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const mix = require('laravel-mix')
 
 mix.js('resources/js/app.js', 'public/js').
     sass('resources/sass/app.scss', 'public/css').
-    version()
+    copy('resources/font', 'public/fonts').
+    version(['public/js', 'public/css'])
