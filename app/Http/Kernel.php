5c53e51be9fab65551413a8b8ad3a59ed25c22d3
kawax <kawaxbiz@gmail.com>
2019-03-25 14:23:57 +0900

    image cache

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
 
         'image' => [
             'bindings',
-            'cache.headers:etag;max_age=300;s_maxage=300'
+            'cache.headers:etag;max_age=3600;s_maxage=3600'
         ],
     ];
 
