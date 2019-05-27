4e07f6ee251565953692276a06bc059cf3dadc31
kawax <kawaxbiz@gmail.com>
2019-05-27 21:22:55 +0900

    server-push

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \TomSchlick\ServerPush\Http2ServerPushMiddleware::class,
+            \Revolution\ServerPush\ServerPush::class,
         ],
 
         'api' => [
