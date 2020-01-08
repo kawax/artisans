b5d71e0a43038c015e7c69d586a44934b65d0f50
kawax <kawaxbiz@gmail.com>
2020-01-08 11:39:00 +0900

    Use CommonMark

--- app/Support/Markdown.php
+++ app/Support/Markdown.php
@@ -3,7 +3,10 @@
 namespace App\Support;
 
 use Illuminate\Support\HtmlString;
-use Parsedown;
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment;
+use League\CommonMark\Ext\Table\TableExtension;
+use League\CommonMark\Ext\Autolink\AutolinkExtension;
 
 class Markdown
 {
@@ -11,12 +14,21 @@ class Markdown
      * Parse the given Markdown text into HTML.
      *
      * @param  string  $text
-     * @return \Illuminate\Support\HtmlString
+     *
+     * @return HtmlString
      */
     public static function parse($text)
     {
-        $parsedown = (new Parsedown())->setSafeMode(true);
+        $environment = Environment::createCommonMarkEnvironment();
 
-        return new HtmlString($parsedown->text($text));
+        $environment->addExtension(new TableExtension());
+        $environment->addExtension(new AutolinkExtension());
+
+        $converter = new CommonMarkConverter([
+            'html_input'         => 'escape',
+            'allow_unsafe_links' => false,
+        ], $environment);
+
+        return new HtmlString($converter->convertToHtml($text));
     }
 }
