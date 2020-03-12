68e5f4f2ad3632a21b4af20b586db4a26a8d7db6
kawax <kawaxbiz@gmail.com>
2020-03-12 09:16:42 +0900

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -46,12 +46,12 @@ return [
     /*
      * Sets the Access-Control-Expose-Headers response header.
      */
-    'exposed_headers' => false,
+    'exposed_headers' => [],
 
     /*
      * Sets the Access-Control-Max-Age response header.
      */
-    'max_age' => false,
+    'max_age' => 0,
 
     /*
      * Sets the Access-Control-Allow-Credentials header.
