b656b92f964e18ed092038c29bc3c6554d8e849e
kawax <kawaxbiz@gmail.com>
2020-09-07 11:07:33 +0900

    [WIP] Wait until Laravel8

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\Profile\MeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,50 +17,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::namespace('Auth')->group(function () {
-    Route::get('login', 'LoginController@login')
-         ->name('login');
-    Route::get('callback', 'LoginController@callback')
-         ->name('callback');
-    Route::any('logout', 'LoginController@logout')
-         ->name('logout');
-});
+Route::namespace('Auth')->group(
+    function () {
+        Route::get('login', [LoginController::class, 'login'])
+             ->name('login');
+        Route::get('callback', [LoginController::class, 'callback'])
+             ->name('callback');
+        Route::any('logout', [LoginController::class, 'logout'])
+             ->name('logout');
+    }
+);
 
-Route::get('@{user:name}', 'UserController')->name('user');
+Route::get('@{user:name}', [UserController::class, '__invoke'])->name('user');
 Route::view('user', 'user.index')->name('user.index');
 
 Route::get('tag/{tag:tag}', 'TagController')->name('tag');
 
 Route::prefix('profile')
-    ->name('profile.')
+     ->name('profile.')
      ->namespace('Profile')
      ->middleware('auth')
-     ->group(function () {
-         Route::view('/', 'profile.edit')
-              ->name('edit');
-         Route::get('/me', 'MeController')
-              ->name('me');
-         Route::put('/', 'UpdateController')
-              ->name('update');
+     ->group(
+         function () {
+             Route::view('/', 'profile.edit')
+                  ->name('edit');
+             Route::get('/me', 'MeController')
+                  ->name('me');
+             Route::put('/', 'UpdateController')
+                  ->name('update');
 
-         Route::view('destroy', 'profile.destroy')
-              ->name('destroy');
-         Route::delete('destroy', 'DestroyController')
-              ->name('delete');
-     });
+             Route::view('destroy', 'profile.destroy')
+                  ->name('destroy');
+             Route::delete('destroy', 'DestroyController')
+                  ->name('delete');
+         }
+     );
 
-Route::namespace('Post')->group(function () {
-    Route::get('post/edit/{post}', 'EditController')
-         ->middleware('can:update,post');
+Route::namespace('Post')->group(
+    function () {
+        Route::get('post/edit/{post}', 'EditController')
+             ->middleware('can:update,post');
 
-    Route::get('post/confirm/{post}', 'ConfirmController')
-         ->name('post.confirm')
-         ->middleware('can:delete,post');
+        Route::get('post/confirm/{post}', 'ConfirmController')
+             ->name('post.confirm')
+             ->middleware('can:delete,post');
 
-    Route::post('post/report/{post}', 'ReportController')
-         ->name('post.report')
-         ->middleware('auth');
-});
+        Route::post('post/report/{post}', 'ReportController')
+             ->name('post.report')
+             ->middleware('auth');
+    }
+);
 
 Route::resource('post', 'PostController');
 
