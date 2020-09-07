15382f2e9f1744443003d51d4c86d187232165d4
kawax <kawax@users.noreply.github.com>
2020-09-07 02:08:16 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\TagController;
 use App\Http\Controllers\Profile\MeController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
