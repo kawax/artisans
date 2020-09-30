9584f78c6884c9e6761e7dd62a5caddc1c770685
kawax <kawaxbiz@gmail.com>
2020-09-30 16:38:31 +0900

    wordwrap

--- app/Providers/MacroServiceProvider.php
+++ app/Providers/MacroServiceProvider.php
@@ -2,20 +2,12 @@
 
 namespace App\Providers;
 
+use App\Support\Mixins\WordWrap;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 
 class MacroServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-    }
-
     /**
      * Bootstrap services.
      *
@@ -23,23 +15,6 @@ class MacroServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Str::macro(
-            'wordwrap',
-            function ($str, $width = 10, $break = PHP_EOL) {
-                $arr = Str::of(mb_convert_kana($str, 'KVa'))
-                          ->split('/\B/u')
-                          ->chunk($width)
-                          ->mapSpread(
-                              function (...$strings) {
-                                  $collect = collect($strings);
-                                  $collect->pop();
-
-                                  return $collect->implode('');
-                              }
-                          );
-
-                return $arr->implode($break);
-            }
-        );
+        Str::mixin(new WordWrap());
     }
 }
---
+++ app/Support/Mixins/WordWrap.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Support\Mixins;
+
+use Illuminate\Support\Str;
+
+class WordWrap
+{
+    /**
+     * @param  string  $str
+     * @param  integer  $width
+     * @param  string  $break
+     *
+     * @return callable
+     */
+    public function wordwrap()
+    {
+        return function (string $str, int $width = 10, string $break = PHP_EOL) {
+            return Str::of(mb_convert_kana($str, 'KVa'))
+                      ->split('/\B/u')
+                      ->chunk($width)
+                      ->mapSpread(
+                          function (...$strings) {
+                              $collect = collect($strings);
+                              $collect->pop();
+
+                              return $collect->implode('');
+                          }
+                      )->implode($break);
+        };
+    }
+}
