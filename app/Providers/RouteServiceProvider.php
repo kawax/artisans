882fb6220780b7f40e872643c5933683ebd0c099
kawax <kawaxbiz@gmail.com>
2021-04-07 19:05:21 +0900

    rate

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -62,11 +62,11 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60);
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
 
         RateLimiter::for('web', function (Request $request) {
-            return $request->user() ? Limit::none() : Limit::perMinutes(10, 60)->by($request->ip());
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
 }
